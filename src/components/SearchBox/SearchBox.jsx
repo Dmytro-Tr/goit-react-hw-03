33f1ae2723ba8ef95458ca34e26ef25d76b92ea8
Dmtro_Tr <Di4gamegem@gmail.com>
2024-12-20 21:43:57 +0200

    step 2 structure

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,16 @@
+import s from "./searchbox.module.css";
+
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  return (
+    <div>
+      <p>Find contact by name</p>
+      <input
+        type="text"
+        className={s.input}
+        placeholder="What name do you want to find?"
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
