7cfb91f36d224edbc86f8b142a155480df67506c
Dmtro_Tr <Di4gamegem@gmail.com>
2024-12-21 19:48:50 +0200

    step 1 localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
 const App = () => {
-  const [contacts, setContacts] = useState([
+  const testContacts = [
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  ];
+
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : testContacts;
+  });
 
   return (
     <div>
